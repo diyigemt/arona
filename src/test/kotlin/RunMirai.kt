158551952a919af5d2637a363de8f69fece14ab8
Haythem <1328343252@qq.com>
2022-07-10 16:36:32 +0800

    添加ignore 优化Wikiru接口

--- src/test/kotlin/RunMirai.kt
+++ src/test/kotlin/RunMirai.kt
@@ -20,10 +20,7 @@ suspend fun main() {
   pluginInstance.load() // 主动加载插件, Console 会调用 Dice.onLoad
   pluginInstance.enable() // 主动启用插件, Console 会调用 Dice.onEnable
 
-//  val properties = Properties().apply { File("account.properties").inputStream().use { load(it) } }
-  val properties = Properties()
-  properties.setProperty("id", "1741557205")
-  properties.setProperty("password", "Text1234")
+  val properties = Properties().apply { File("account.properties").inputStream().use { load(it) } }
 
   val bot = MiraiConsole.addBot(properties.getProperty("id").toLong(), properties.getProperty("password"))
     .alsoLogin() // 登录一个测试环境的 Bot

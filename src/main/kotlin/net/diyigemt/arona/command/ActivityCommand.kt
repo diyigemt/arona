fe0cd101a059d0da8bb8aaad5bf92ac7fb2a098d
diyigemt <qwe1355247243@gmail.com>
2022-07-13 08:55:49 +0800

    移除多余服务判断

--- src/main/kotlin/net/diyigemt/arona/command/ActivityCommand.kt
+++ src/main/kotlin/net/diyigemt/arona/command/ActivityCommand.kt
@@ -70,7 +70,7 @@ object ActivityCommand : CompositeCommand(
    private val ACTIVITY_COMMAND = "${CommandManager.commandPrefix}活动"
    override fun interceptBeforeCall(message: Message, caller: CommandSender): String? {
      if (message.contentToString() != ACTIVITY_COMMAND) return null
-     if ((caller is UserCommandSender) && GeneralUtils.checkService(caller.subject)) {
+     if (caller is UserCommandSender) {
        val subject = caller.subject
        if (AronaNotifyConfig.defaultActivityCommandServer == ServerLocale.JP) {
          kotlin.runCatching {

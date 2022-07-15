351130938b6cb78422d459e62b6b18a1b3d66173
Haythem <1328343252@qq.com>
2022-07-15 16:33:42 +0800

    修复wikiru重复添加活动信息的bug

--- src/test/kotlin/RunMirai.kt
+++ src/test/kotlin/RunMirai.kt
@@ -7,6 +7,7 @@ import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import java.io.File
 import java.util.*
 
 @OptIn(ConsoleExperimentalApi::class)
@@ -18,10 +19,7 @@ suspend fun main() {
   pluginInstance.load() // 主动加载插件, Console 会调用 Dice.onLoad
   pluginInstance.enable() // 主动启用插件, Console 会调用 Dice.onEnable
 
-//  val properties = Properties().apply { File("account.properties").inputStream().use { load(it) } }
-  val properties = Properties()
-  properties.setProperty("id", "1741557205")
-  properties.setProperty("password", "Text1234")
+  val properties = Properties().apply { File("account.properties").inputStream().use { load(it) } }
 
   val bot = MiraiConsole.addBot(properties.getProperty("id").toLong(), properties.getProperty("password"))
     .alsoLogin() // 登录一个测试环境的 Bot

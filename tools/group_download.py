2deaa1ba1c414298e31480241058d12e0d089323
diyigemt <qwe1355247243@gmail.com>
2022-10-15 16:19:13 +0800

    save

--- tools/group_download.py
+++ tools/group_download.py
@@ -1,14 +1,16 @@
 from tools import draw_image
 
 images = [{
-    "name": "火力演习.png",
+    "name": "日服角色简评.png",
     "path": "./image/some/",
-    "url": "https://i0.hdslb.com/bfs/new_dyn/0a5afeff2c8108817be5d62472c8ccf1425535005.png@1295w.webp",
-}, {
-    "name": "日服火力演习.png",
-    "path": "./image/some/",
-    "url": "https://i0.hdslb.com/bfs/new_dyn/67bffb5b62399cb0739cab9d1aea05a5425535005.png@1295w.webp",
-}]
+    "url": "https://i0.hdslb.com/bfs/article/cbbbc5d481129f58c7cacf671bfc0772c2cb6bb8.png@942w_711h_progressive.webp",
+}
+# ,{
+#     "name": "纱织.png",
+#     "path": "./image/student_rank/",
+#     "url": "https://i0.hdslb.com/bfs/new_dyn/473218dca85027dbbe763eb3b19d31d9425535005.png@1295w.webp",
+# }
+]
 
 if __name__ == "__main__":
     count = 1

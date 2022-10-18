b36516f3fd6a56c950deee32a77a63d74e8bbf7b
diyigemt <qwe1355247243@gmail.com>
2022-10-18 11:09:43 +0800

    i8n

--- arona-webui-frontend/src/views/home/HomeIndex.vue
+++ arona-webui-frontend/src/views/home/HomeIndex.vue
@@ -1,5 +1,22 @@
-<template>this is home</template>
+<template>
+  <el-form :model="formData">
+    <el-form-item prop="api" label="api:">
+      <el-input v-model="formData.api" />
+    </el-form-item>
+  </el-form>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface FormData {
+  api: string;
+  params: any;
+  response: any;
+}
+const formData = reactive<FormData>({
+  api: "",
+  params: {},
+  response: {},
+});
+</script>
 
 <style lang="scss" scoped></style>

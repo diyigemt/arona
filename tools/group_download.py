c393229bbd13e7afb21e6b2632d515fcbfd9186d
diyigemt <qwe1355247243@gmail.com>
2022-10-22 12:25:59 +0800

    再次优化上传方法

--- tools/group_download.py
+++ tools/group_download.py
@@ -1,21 +1,19 @@
+import yaml
+from yaml.loader import SafeLoader
 from tools import draw_image
 
-images = [{
-    "name": "火力演习.png",
-    "path": "./image/some/",
-    "url": "https://i0.hdslb.com/bfs/new_dyn/0a5afeff2c8108817be5d62472c8ccf1425535005.png@1295w.webp",
-}, {
-    "name": "日服火力演习.png",
-    "path": "./image/some/",
-    "url": "https://i0.hdslb.com/bfs/new_dyn/67bffb5b62399cb0739cab9d1aea05a5425535005.png@1295w.webp",
-}]
-
 if __name__ == "__main__":
+    source = {}
+    with open(r"./group_download.yml", "r", encoding="UTF-8") as f:
+        source = yaml.load(f, SafeLoader)
     count = 1
+    images = list(filter(lambda item: item['name'] != 'test', list(source['image'])))
     total = len(images)
     for item in images:
         print("%d/%d" % (count, total))
         name = item["name"]
+        if name.find(".png") == -1:
+            name = name + ".png"
         path = item["path"]
         url = item["url"].split("@")[0]
         draw_image(url, name, path)

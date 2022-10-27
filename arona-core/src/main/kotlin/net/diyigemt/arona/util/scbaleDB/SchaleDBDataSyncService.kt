71c85f78cab92c178f056286ad23e4b632f7df6c
Haythem <1328343252@qq.com>
2022-10-27 15:59:02 +0800

    修复数据库在失败时仍更新的bug，修改部分表的列长度

--- arona-core/src/main/kotlin/net/diyigemt/arona/util/scbaleDB/SchaleDBDataSyncService.kt
+++ arona-core/src/main/kotlin/net/diyigemt/arona/util/scbaleDB/SchaleDBDataSyncService.kt
@@ -8,6 +8,7 @@ import net.diyigemt.arona.db.data.schaledb.MD5
 import net.diyigemt.arona.entity.schaleDB.*
 import net.diyigemt.arona.quartz.QuartzProvider
 import net.diyigemt.arona.service.AronaQuartzService
+import okhttp3.internal.wait
 import okio.ByteString.Companion.toByteString
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.insert
@@ -133,16 +134,18 @@ object SchaleDBDataSyncService : AronaQuartzService{
         RemoteType.GITHUB.name -> apply {
           resString += " from GitHub"
           if (query.getOrNull(MD5.md5) != md5 && isGitHub){
-            dao.sendToDataBase()
-            updateMD5(dataType, RemoteType.GITHUB.name, md5)
+            kotlin.runCatching { dao.sendToDataBase() }.onSuccess {
+              updateMD5(dataType, RemoteType.GITHUB.name, md5)
+            }
             isUpdate = true
           } else resString += " already up to date."
         }
         else -> apply {
           resString += " from mirror"
           if (query.getOrNull(MD5.md5) != md5){
-            dao.sendToDataBase()
-            updateMD5(dataType, RemoteType.MIRROR.name, md5)
+            kotlin.runCatching { dao.sendToDataBase() }.onSuccess {
+              updateMD5(dataType, RemoteType.MIRROR.name, md5)
+            }
             isUpdate = true
           } else resString += " already up to date."
         }

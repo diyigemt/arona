c19199f329bedf70f642431ee7e7d23edcbac2ff
Haythem <1328343252@qq.com>
2022-11-03 18:46:19 +0800

    阶段性提交

--- arona-core/src/main/kotlin/net/diyigemt/arona/web/api/v1/data/Data.kt
+++ arona-core/src/main/kotlin/net/diyigemt/arona/web/api/v1/data/Data.kt
@@ -5,13 +5,11 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.util.pipeline.*
-import net.diyigemt.arona.Arona
 import net.diyigemt.arona.util.MoshiUtil
 import net.diyigemt.arona.web.api.v1.Worker
 import net.diyigemt.arona.web.api.v1.message.DBJob
 import net.diyigemt.arona.web.api.v1.message.ServerResponse
 import net.diyigemt.arona.web.database.DBOptionService
-import org.jetbrains.exposed.sql.Expression
 
 /**
  *@Author hjn
@@ -21,11 +19,10 @@ object Data : Worker {
   override suspend fun worker(context: PipelineContext<Unit, ApplicationCall>) {
     super.worker(context)
     kotlin.runCatching {
-      val json = json.decodeFromString(DBJob.serializer(), context.call.receiveText())
+      val json = MoshiUtil.reflect.adapter(DBJob::class.java).fromJson(context.call.receiveText())!!
       json.properties.db = context.call.parameters["dataBase"]!!
       json.properties.table = context.call.parameters["table"]!!
       val res = DBOptionService.addTask(json)
-      //Arona.info(res.toString())
       val type = Types.newParameterizedType(ServerResponse::class.java, Any::class.java)
       context.call.respond(MoshiUtil.reflect.adapter<ServerResponse<out Any>>(type).serializeNulls().toJson(res))
     }.onFailure {

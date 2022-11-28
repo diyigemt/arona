f9d65903f4ab4779229a470b777ba07de866fa61
diyigemt <qwe1355247243@gmail.com>
2022-11-28 14:17:11 +0800

    材料一览

---
+++ tools/generate_game_db_overview.py
@@ -0,0 +1,22 @@
+import time
+from playwright.sync_api import Playwright, sync_playwright
+
+def run(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=True, slow_mo=100)
+    context = browser.new_context(viewport={'width': 1920, 'height': 1080}, device_scale_factor=4.0)
+    page = context.new_page()
+
+    # 拿到成长资源截图
+    page.goto("https://ba.game-db.tw/")
+    page.locator("svg").click()
+    page.locator("#react-select-2-option-0").click()
+    page.locator("text=早見表 >> nth=1").click()
+    time.sleep(2)
+    page.screenshot(path="./image/材料一览.png")
+    page.close()
+    context.close()
+    browser.close()
+
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        run(playwright)

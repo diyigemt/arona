8f7f0b45477ab1c71f7b5566773b78d4c1231b76
Haythem <1328343252@qq.com>
2023-01-27 22:50:36 +0800

    根积木块突变器实现

--- arona-webui-frontend/src/blockly/images.ts
+++ arona-webui-frontend/src/blockly/images.ts
@@ -28,3 +28,9 @@ export const plus =
   "FjLTEuMTA0IDAtMiAuODk2LTIgMnMuODk2IDIgMiAybDQuMDcxLS4wNzEtLjA3MSA0LjA3MW" +
   "MwIDEuMTA0Ljg5NiAyIDIgMnMyLS44OTYgMi0ydi00LjA3MWw0IC4wNzFjMS4xMDQgMCAyLS" +
   "44OTYgMi0ycy0uODk2LTItMi0yeiIgZmlsbD0id2hpdGUiIC8+PC9zdmc+Cg==";
+
+export const minus =
+  "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC" +
+  "9zdmciIHZlcnNpb249IjEuMSIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0Ij48cGF0aCBkPSJNMT" +
+  "ggMTFoLTEyYy0xLjEwNCAwLTIgLjg5Ni0yIDJzLjg5NiAyIDIgMmgxMmMxLjEwNCAwIDItLj" +
+  "g5NiAyLTJzLS44OTYtMi0yLTJ6IiBmaWxsPSJ3aGl0ZSIgLz48L3N2Zz4K";
---
+++ arona-webui-frontend/src/blockly/mutators/MasterBlockMutator.ts
@@ -0,0 +1,26 @@
+import AbstractMutator, { MutatorBlockSvg } from "@/blockly/mutators/AbstractMutator";
+
+export default class MasterBlockMutator extends AbstractMutator {
+  controlsHelper(this: MutatorBlockSvg): void {
+    this.getInput("Expressions")!.insertFieldAt(0, MasterBlockMutator.createPlusField(), "plus");
+  }
+
+  addPart(this: MutatorBlockSvg) {
+    this.itemCount++;
+    this.appendValueInput(`Expressions${this.itemCount}`)
+      .appendField(MasterBlockMutator.createMinusField(), "minus")
+      .appendField("                                当");
+
+    this.appendStatementInput(`Actions${this.itemCount}`).appendField("执行");
+  }
+
+  removePart(this: MutatorBlockSvg) {
+    if (this.itemCount !== 0) {
+      const input = this.getInput(`Expressions${this.itemCount}`)!.connection;
+      if (input?.isConnected()) input?.disconnect();
+      this.removeInput(`Expressions${this.itemCount}`);
+      this.removeInput(`Actions${this.itemCount}`);
+      this.itemCount--;
+    }
+  }
+}

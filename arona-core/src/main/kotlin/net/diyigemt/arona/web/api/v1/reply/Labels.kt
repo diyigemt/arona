9ad0b4baa12c5069301c8a1160baa8d899b840cf
Haythem <1328343252@qq.com>
2023-02-21 10:37:25 +0800

    标签的get和create

--- arona-core/src/main/kotlin/net/diyigemt/arona/web/api/v1/reply/Labels.kt
+++ arona-core/src/main/kotlin/net/diyigemt/arona/web/api/v1/reply/Labels.kt
@@ -6,16 +6,16 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.util.pipeline.*
 import kotlinx.serialization.Serializable
-import net.diyigemt.arona.Arona
 import net.diyigemt.arona.db.DataBaseProvider
 import net.diyigemt.arona.db.reply.LabelItem
 import net.diyigemt.arona.db.reply.Labels
 import net.diyigemt.arona.web.api.v1.Worker
 import net.diyigemt.arona.web.api.v1.message.ServerResponse
 import net.diyigemt.arona.web.api.v1.responseMessage
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.update
 
 /**
  *@Author hjn
@@ -28,23 +28,41 @@ object Labels: Worker {
       HttpMethod.Get -> context.call.respond(responseMessage(getLabelItems()))
       HttpMethod.Post -> {
         val receive = context.call.receiveText()
+
         when(context.call.parameters["method"]) {
-          "create" -> {
+          "create" -> kotlin.runCatching {
             val json = json.decodeFromString(RequestLabel.serializer(), receive).label
-            kotlin.runCatching {
-              DataBaseProvider.query {
-                Labels.insert {
-                  it[value] = json.value
-                  it[weight] = json.weight
-                }
+            DataBaseProvider.query {
+              Labels.insert {
+                it[value] = json.value
+                it[weight] = json.weight
               }
-            }.onFailure {
-              it.printStackTrace()
-              context.call.respond(ServerResponse(500, HttpStatusCode.InternalServerError.description, ""))
-              return
             }
-            context.call.respond(responseMessage(getLabelItems()))
-          }
+          }.onFailure {
+            it.printStackTrace()
+            context.call.respond(ServerResponse(500, HttpStatusCode.InternalServerError.description, ""))
+          }.onSuccess { context.call.respond(responseMessage("")) }
+          "update" -> kotlin.runCatching {
+            val json = json.decodeFromString(RequestLabel.serializer(), receive).label
+            DataBaseProvider.query {
+              Labels.update({Labels.id eq json.id}) {
+                it[value] = json.value
+                it[weight] = json.weight
+              }
+            }
+          }.onFailure {
+            it.printStackTrace()
+            context.call.respond(ServerResponse(500, HttpStatusCode.InternalServerError.description, ""))
+          }.onSuccess { context.call.respond(responseMessage("")) }
+          "delete" -> kotlin.runCatching {
+            val json = json.decodeFromString(DeleteRequest.serializer(), receive)
+            DataBaseProvider.query {
+              Labels.deleteWhere { Labels.id eq json.id }
+            }
+          }.onFailure {
+            it.printStackTrace()
+            context.call.respond(ServerResponse(500, HttpStatusCode.InternalServerError.description, ""))
+          }.onSuccess { context.call.respond(responseMessage("")) }
         }
       }
     }
@@ -63,4 +81,7 @@ object Labels: Worker {
 
   @Serializable
   data class RequestLabel(val label: LabelItem)
+
+  @Serializable
+  data class DeleteRequest(val id: Int)
 }

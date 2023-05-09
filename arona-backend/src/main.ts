51a0d74845cb6fb6f84524864f4b4e5b4ad820fe
diyigemt 111 <qwe1355247243@gmail.com>
2023-05-09 15:27:10 +0800

    chore: 优化env加载

--- arona-backend/src/main.ts
+++ arona-backend/src/main.ts
@@ -1,5 +1,7 @@
 import * as dotenv from "dotenv";
-dotenv.config();
+dotenv.config({
+  path: process.env.NODE_ENV === "development" ? ".env" : ".env.product",
+});
 import { Logger } from "./utils/logger";
 import http from "http";
 import fs from "fs";

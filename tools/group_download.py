796db6ff4dad6f531f31dfb138bfecaab0f52a99
diyigemt <qwe1355247243@gmail.com>
2024-05-07 23:46:01 +0800

    feat(tools): 新增数据源

--- tools/group_download.py
+++ tools/group_download.py
@@ -11,6 +11,7 @@ source_map = {
     "2": "bilbilibili@朝夕desu(4607471)",
     "3": "bilbilibili@y千代(49533273)",
     "4": "bilbilibili@蔚蓝档案(3493265644980448)",
+    "5": "bilbilibili@白夜清露(35434465)",
 }
 
 if __name__ == "__main__":

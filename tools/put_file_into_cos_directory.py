dc4768bdf0009750b9b3f8e72a6086923908ef34
diyigemt <qwe1355247243@gmail.com>
2026-02-04 17:48:39 +0800

    feat(tools): 五周年fes

--- tools/put_file_into_cos_directory.py
+++ tools/put_file_into_cos_directory.py
@@ -1,56 +1,56 @@
-import os
-import getpass
-import json
-import codecs
-import shutil
-from qcloud_cos import CosConfig
-from qcloud_cos import CosS3Client
-from qcloud_cos.cos_threadpool import SimpleThreadPool
+# import os
+# import getpass
+# import json
+# import codecs
+# import shutil
+# from qcloud_cos import CosConfig
+# from qcloud_cos import CosS3Client
+# from qcloud_cos.cos_threadpool import SimpleThreadPool
 
-from tools import confirm_action, safe_move
+# from tools import confirm_action, safe_move
 
-COS_ID = ""
-COS_NAME = ""
-SECRET_ID = ""
-SECRET_KEY = ""
-Bucket = ""
-REGION = "ap-shanghai"
-BASE_FOLDER = "image"
+# COS_ID = ""
+# COS_NAME = ""
+# SECRET_ID = ""
+# SECRET_KEY = ""
+# Bucket = ""
+# REGION = "ap-shanghai"
+# BASE_FOLDER = "image"
 
-if __name__ == "__main__":
-    if os.path.exists(r"C:\Users\%s\.ssh\arona-cos.json" % getpass.getuser()):
-        with codecs.open(r"C:\Users\%s\.ssh\arona-cos.json" % getpass.getuser(), "r", encoding="utf-8") as f:
-            read = json.loads(f.read())
-            COS_ID = read["COS_ID"]
-            COS_NAME = read["COS_NAME"]
-            SECRET_ID = read["SECRET_ID"]
-            SECRET_KEY = read["SECRET_KEY"]
-            Bucket = COS_NAME + "-" + COS_ID 
-    else:
-        print("arona-cos.json not found.")
-        exit(0)
-    config = CosConfig(Region=REGION, SecretId=SECRET_ID, SecretKey=SECRET_KEY)
-    client = CosS3Client(config)
-    pool = SimpleThreadPool()
-    action_list = [] # file_name local_path cos_path
-    for root, dirs, files in os.walk("./raw"):
-        if root.startswith("./raw\\_history"):
-            continue
-        base_path = root.replace("\\", "/").replace("./raw", "file")
-        for file in files:
-            action_list.append((file, root.replace("\\", "/") + "/" + file, base_path + "/" + file))
-    print("list: %s" % ", ".join(list(map(lambda tu: tu[0], action_list))))
-    if not confirm_action():
-        exit(0)
-    for item in action_list:
-        pool.add_task(client.upload_file, Bucket, item[2], item[1])
-    pool.wait_completion()
-    # 移动到history文件夹
-    for item in action_list:
-        local_path = item[1]
-        file_history_path = str(local_path).replace("raw", "raw/_history")
-        parent_path = file_history_path[:file_history_path.rfind("/")]
-        if not os.path.exists(parent_path):
-            os.makedirs(file_history_path[:file_history_path.rfind("/")])
-        safe_move(local_path, file_history_path)
-    pass
+# if __name__ == "__main__":
+#     if os.path.exists(r"C:\Users\%s\.ssh\arona-cos.json" % getpass.getuser()):
+#         with codecs.open(r"C:\Users\%s\.ssh\arona-cos.json" % getpass.getuser(), "r", encoding="utf-8") as f:
+#             read = json.loads(f.read())
+#             COS_ID = read["COS_ID"]
+#             COS_NAME = read["COS_NAME"]
+#             SECRET_ID = read["SECRET_ID"]
+#             SECRET_KEY = read["SECRET_KEY"]
+#             Bucket = COS_NAME + "-" + COS_ID 
+#     else:
+#         print("arona-cos.json not found.")
+#         exit(0)
+#     config = CosConfig(Region=REGION, SecretId=SECRET_ID, SecretKey=SECRET_KEY)
+#     client = CosS3Client(config)
+#     pool = SimpleThreadPool()
+#     action_list = [] # file_name local_path cos_path
+#     for root, dirs, files in os.walk("./raw"):
+#         if root.startswith("./raw\\_history"):
+#             continue
+#         base_path = root.replace("\\", "/").replace("./raw", "file")
+#         for file in files:
+#             action_list.append((file, root.replace("\\", "/") + "/" + file, base_path + "/" + file))
+#     print("list: %s" % ", ".join(list(map(lambda tu: tu[0], action_list))))
+#     if not confirm_action():
+#         exit(0)
+#     for item in action_list:
+#         pool.add_task(client.upload_file, Bucket, item[2], item[1])
+#     pool.wait_completion()
+#     # 移动到history文件夹
+#     for item in action_list:
+#         local_path = item[1]
+#         file_history_path = str(local_path).replace("raw", "raw/_history")
+#         parent_path = file_history_path[:file_history_path.rfind("/")]
+#         if not os.path.exists(parent_path):
+#             os.makedirs(file_history_path[:file_history_path.rfind("/")])
+#         safe_move(local_path, file_history_path)
+#     pass
